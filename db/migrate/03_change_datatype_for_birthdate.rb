73e51aab44ad573d8d4462179ef855ae30015eae
David McGrann <dpmcgrann@gmail.com>
2018-07-15 19:50:35 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatype < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change_column
     chagne_column :students, :birthdate, :datetime
